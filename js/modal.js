a3e2882b293786532b5daf8b3b7006fd759a062e
Jairo Filho <jairofilho79@gmail.com>
2023-04-13 09:56:59 -0300

    Adding click event only when open modal

--- js/modal.js
+++ js/modal.js
@@ -3,25 +3,28 @@ const modal = document.querySelector("dialog");
 const buttonClose = document.querySelector("#fecharModal")
 const modalBody = document.querySelector("#modal-container")
 
+const windowListener = function(e) {
+  console.log(e)
+  const lugarDoClick = e.target
+  if (!modalBody.contains(lugarDoClick)){
+    fecharModal()
+  }
+}
+
 button.onclick = (e) => {
   abrirModal()
 }
 
-buttonClose.onclick = (e) => {
+buttonClose.onclick = (event) => {
   fecharModal()
 }
 
 function abrirModal () {
   modal.showModal()
+  setTimeout(() => {window.addEventListener('click', windowListener, false)}, 0)
 }
 
 function fecharModal () {
   modal.close()
+  window.removeEventListener('click', windowListener, false)
 }
-
-window.addEventListener('click', function(e) {
-  if(button.contains(e.target)) return
-  if (modal.hasAttribute('open') && !modalBody.contains(e.target)){
-    fecharModal()
-  }
-});
